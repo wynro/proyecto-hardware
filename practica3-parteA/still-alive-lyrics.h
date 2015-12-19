ee5e80eba3f238a33394372d19b3258ee20d26c2
Guillermo Robles <guillerobles1995@gmail.com>
2015-12-19 12:05:15 +0100

    pr3-ptA: Still Alive documented

--- practica3-parteA/still-alive-lyrics.h
+++ practica3-parteA/still-alive-lyrics.h
@@ -1,14 +1,14 @@
-/*
- * still-alive-lyrics.h
- *
- *  Created on: 10/12/2015
- *      Author: a604409
+/**
+ * @file still-alive-lyrics.h
+ * @brief Archivo de recursos con los creditos finales
+ * @author Guillermo Robles
  */
-
 #ifndef STILL_ALIVE_LYRICS_H_
 #define STILL_ALIVE_LYRICS_H_
 
 #define STILL_ALIVE_SIZE 117
+
+// Todos los derechos rservados a sus respectivos dueños 
 
 char *still_alive[STILL_ALIVE_SIZE];
 #endif /* STILL_ALIVE_LYRICS_H_ */
